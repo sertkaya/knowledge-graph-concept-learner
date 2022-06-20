9eed9170116f1d656ac9bcdf40bb3340e2a04710
Baris Sertkaya <3862859+sertkaya@users.noreply.github.com>
2022-06-20 17:41:32 +0200

    Added to_str, is_homomorphic_to, is_equivalent_to.

--- description_tree.py
+++ description_tree.py
@@ -1,8 +1,9 @@
-from rdflib import Graph, URIRef
+from rdflib import URIRef
 import copy
 
 RDF_TYPE = URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
 OWL_THING = URIRef("http://www.w3.org/2002/07/owl#thing")
+OWL_NOTHING = URIRef("http://www.w3.org/2002/07/owl#nothing")
 
 
 class DescriptionTree:
@@ -67,39 +68,92 @@ class DescriptionTree:
             for t in self.edges.get(edge):
                 t.print(n + 1, g)
 
+    def to_str(self, g):
+        s = ""
+        len_labels = len(self.labels)
+        i = 0
+        for l in self.labels:
+            if l == OWL_THING:
+                s += "⊤"
+            elif l == OWL_NOTHING:
+                s += "⊥"
+            else:
+                s += l.n3(g.namespace_manager)
+            if i < (len_labels - 1):
+                s += " ⊓ "
+            i += 1
 
-base_URI = "http://example.org/"
-x1 = URIRef(base_URI + "x1")
-x2 = URIRef(base_URI + "x2")
-x3 = URIRef(base_URI + "x3")
-x4 = URIRef(base_URI + "x4")
+        keys = self.edges.keys()
+        len_keys = len(keys)
+        if len_keys != 0 and len_labels != 0:
+            s += " ⊓ "
+        i = 0
+        for edge in keys:
+            s += "∃" + edge.n3(g.namespace_manager) + ".("
+            for t in self.edges.get(edge):
+                s += t.to_str(g)
+            s += ")"
+            if i < (len_keys - 1):
+                s += " ⊓ "
+            i += 1
+        return s
 
-g = Graph()
-g.parse("test/example-product-11.ttl")
-g.bind("ex", base_URI)
+    def is_homomorphic_to(self, t):
+        if len(self.edges) == 0:
+            return self.labels.issubset(t.labels)
+        else:
+            for e in self.edges.keys():
+                if e in t.edges:
+                    for c1 in self.edges.get(e):
+                        child_homomorphic = False
+                        for c2 in t.edges.get(e):
+                            if c1.is_homomorphic_to(c2):
+                                child_homomorphic = True
+                                break
+                        if not child_homomorphic:
+                            return False
+                else:
+                    return False
+            return True
 
-t1 = DescriptionTree(g)
-t1.unravel(x1, 1)
-print("t1:")
-t1.print(0, g)
+    def is_subsumed_by(self, t):
+        return t.is_homomorphic_to(self)
 
-t2 = DescriptionTree(g)
-t2.unravel(x2, 1)
-print("t2:")
-t2.print(0, g)
-
-t3 = DescriptionTree(g)
-t3.unravel(x3, 1)
-print("t3:")
-t3.print(0, g)
-
-# t4 = DescriptionTree(g)
-# t4.unravel(x4, 1)
-# print("t4:")
-# t4.print(0, g)
-
-# p = t1.product({t2, t3, t4})
-p = t1.product({t2, t3})
-
-print("Product:")
-p.print(0, g)
+    def is_equivalent_to(self, t):
+        return t.is_homomorphic_to(self) and self.is_homomorphic_to(t)
+#
+# base_URI = "http://example.org/"
+# x1 = URIRef(base_URI + "x1")
+# x2 = URIRef(base_URI + "x2")
+# x3 = URIRef(base_URI + "x3")
+# x4 = URIRef(base_URI + "x4")
+#
+# g = Graph()
+# g.parse("test/example-product-11.ttl")
+# g.bind("ex", base_URI)
+#
+# t1 = DescriptionTree(g)
+# t1.unravel(x1, 1)
+# print("t1:")
+# t1.print(0, g)
+#
+# t2 = DescriptionTree(g)
+# t2.unravel(x2, 1)
+# print("t2:")
+# t2.print(0, g)
+#
+# t3 = DescriptionTree(g)
+# t3.unravel(x3, 1)
+# print("t3:")
+# t3.print(0, g)
+#
+# # t4 = DescriptionTree(g)
+# # t4.unravel(x4, 1)
+# # print("t4:")
+# # t4.print(0, g)
+#
+# # p = t1.product({t2, t3, t4})
+# p = t1.product({t2, t3})
+#
+# print("Product:")
+# p.print(0, g)
