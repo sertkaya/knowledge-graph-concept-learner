89925ad04411119ad2772ae5446d008046a6e7d9
Baris Sertkaya <3862859+sertkaya@users.noreply.github.com>
2022-06-20 17:41:32 +0200

    Binary and n-ary product of description trees.

--- description_tree.py
+++ description_tree.py
@@ -16,7 +16,7 @@ class DescriptionTree:
         c = DescriptionTree(self.graph)
         c.labels = copy.deepcopy(self.labels)
         c.edges = copy.deepcopy(self.edges)
-        return(c)
+        return c
 
     def unravel(self, node, depth):
         """ Unravels the RDF-Graph at the given node until the given depth. """
@@ -35,17 +35,23 @@ class DescriptionTree:
             self.labels.add(OWL_THING)
             return self
 
-    def product(self, trees, depth):
-        if depth == 0:
-            # product tree
-            pt = self.copy()
-            for tree in trees:
-               pt.labels.intersection_update(tree.labels)
-            return(pt)
-        else:
-            pt = DescriptionTree(self.graph)
-            return(pt)
+    def binary_product(self, t):
+        # product tree
+        p = DescriptionTree(self.graph)
+        p.labels = copy.deepcopy(self.labels)
+        p.labels.intersection_update(t.labels)
+        for e in self.edges.keys():
+            if e in t.edges:
+                for c1 in self.edges.get(e):
+                    for c2 in t.edges.get(e):
+                        p.edges.setdefault(e, set()).add(c1.binary_product(c2))
+        return p
 
+    def product(self, trees):
+        p = self.copy()
+        for t in trees:
+            p = p.binary_product(t)
+        return p
 
     def print(self, n, g):
         """ Prints the description graph. """
@@ -59,26 +65,41 @@ class DescriptionTree:
                 print("\t", end="")
             print(edge.n3(g.namespace_manager))
             for t in self.edges.get(edge):
-                t.print(n+1, g)
+                t.print(n + 1, g)
 
 
 base_URI = "http://example.org/"
 x1 = URIRef(base_URI + "x1")
 x2 = URIRef(base_URI + "x2")
+x3 = URIRef(base_URI + "x3")
+x4 = URIRef(base_URI + "x4")
 
 g = Graph()
-g.parse("test/example-product-1.ttl")
+g.parse("test/example-product-11.ttl")
 g.bind("ex", base_URI)
 
 t1 = DescriptionTree(g)
 t1.unravel(x1, 1)
-
+print("t1:")
 t1.print(0, g)
 
-# t2 = DescriptionTree(g)
-# t2.unravel(x2, 0)
-#
-# p = t1.product({t2}, 0)
-#
-# for t in p.labels:
-#     print(t)
+t2 = DescriptionTree(g)
+t2.unravel(x2, 1)
+print("t2:")
+t2.print(0, g)
+
+t3 = DescriptionTree(g)
+t3.unravel(x3, 1)
+print("t3:")
+t3.print(0, g)
+
+# t4 = DescriptionTree(g)
+# t4.unravel(x4, 1)
+# print("t4:")
+# t4.print(0, g)
+
+# p = t1.product({t2, t3, t4})
+p = t1.product({t2, t3})
+
+print("Product:")
+p.print(0, g)
