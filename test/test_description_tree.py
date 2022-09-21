1e915a562fe485005907daeeb0af224f4e27e047
Baris Sertkaya <3862859+sertkaya@users.noreply.github.com>
2022-09-21 11:27:20 +0200

    Moved the description_tree and kg_mining tests to test.

---
+++ test/test_description_tree.py
@@ -0,0 +1,23 @@
+from rdflib import Graph, URIRef
+from description_tree import DescriptionTree
+
+base_URI = "http://example.org/"
+# x1 = URIRef(base_URI + "x1")
+# x2 = URIRef(base_URI + "x2")
+# x3 = URIRef(base_URI + "x3")
+# x4 = URIRef(base_URI + "x4")
+
+a = URIRef(base_URI + "a")
+b = URIRef(base_URI + "b")
+c = URIRef(base_URI + "c")
+
+g = Graph()
+g.parse("test_files/example-iccs.ttl")
+g.bind("ex", base_URI)
+
+depth = 0
+t1 = DescriptionTree(g)
+t1.unravel(a, depth)
+print("t1:")
+t1.print(0, g)
+print(t1.to_str(g))
